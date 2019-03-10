e22841be2778e9484672c26e813b655694e2be65
Manuel Monforte <manumonforesco2@gmail.com>
2019-03-10 10:02:53 +0100

    data in dataframe

--- Estudio.R
+++ Estudio.R
@@ -1,8 +1,10 @@
 #Archivo uutilziado para recolocar los datos en un nuevo excel
 
 #Instalamos paquetes para agrupar csv
-
 install.packages("data.table")
+
+#Intslamos paquete para manipular ts
+install.packages("xts")
 
 #Cargamos los datos de los anos en un dataset (utilziamos read.csv2 ya que el separador es ;)
 
@@ -26,7 +28,7 @@ data2017<- read.csv(file = "madrid_2017.csv",header = TRUE);
 data2018<- read.csv(file = "madrid_2018.csv",header = TRUE);
 
 #unimos los datos en una lista para luego introducirla en un dataset
-data <- rbind(list(data2001,data2002,data2003,data2004,data2005,data2006,data2007,data2008,data2009,data2010,data2011,data2012,
+data <- data.table::rbindlist(list(data2001,data2002,data2003,data2004,data2005,data2006,data2007,data2008,data2009,data2010,data2011,data2012,
               data2013,data2014,data2015,data2016,data2017,data2018), fill = TRUE);
 
 datasetMadrid  <- as.data.frame(data); 
@@ -38,6 +40,37 @@ tail(datasetMadrid)
 #Introducimos los datos en una serie temporal
 
 
-#Cargamos los datos dfe las precipitaciones
+#Cargamos los datos de las precipitaciones
+rainfall_2001<- readxl::read_xlsx("rainfall_2001.xlsx",col_names = TRUE);
+rainfall_2002<- readxl::read_xlsx("rainfall_2002.xlsx",col_names = TRUE);
+rainfall_2003<- readxl::read_xlsx("rainfall_2003.xlsx",col_names = TRUE);
+rainfall_2004<- readxl::read_xlsx("rainfall_2004.xlsx",col_names = TRUE);
+rainfall_2005<- readxl::read_xlsx("rainfall_2005.xlsx",col_names = TRUE);
+rainfall_2006<- readxl::read_xlsx("rainfall_2006.xlsx",col_names = TRUE);
+rainfall_2007<- readxl::read_xlsx("rainfall_2007.xlsx",col_names = TRUE);
+rainfall_2008<- readxl::read_xlsx("rainfall_2008.xlsx",col_names = TRUE);
+rainfall_2009<- readxl::read_xlsx("rainfall_2009.xlsx",col_names = TRUE);
+rainfall_2010<- readxl::read_xlsx("rainfall_2010.xlsx",col_names = TRUE);
+rainfall_2011<- readxl::read_xlsx("rainfall_2011.xlsx",col_names = TRUE);
+rainfall_2012<- readxl::read_xlsx("rainfall_2012.xlsx",col_names = TRUE);
+rainfall_2013<- readxl::read_xlsx("rainfall_2013.xlsx",col_names = TRUE);
+rainfall_2014<- readxl::read_xlsx("rainfall_2014.xlsx",col_names = TRUE);
+rainfall_2015<- readxl::read_xlsx("rainfall_2015.xlsx",col_names = TRUE);
+rainfall_2016<- readxl::read_xlsx("rainfall_2016.xlsx",col_names = TRUE);
+rainfall_2017<- readxl::read_xlsx("rainfall_2017.xlsx",col_names = TRUE);
+rainfall_2018<- readxl::read_xlsx("rainfall_2018.xlsx",col_names = TRUE);
 
 
+rainfall <- data.table::rbindlist(list(rainfall_2001,rainfall_2002,rainfall_2003,rainfall_2004,
+              rainfall_2005,rainfall_2006,rainfall_2007,rainfall_2008,rainfall_2009,rainfall_2010,
+              rainfall_2011,rainfall_2012,rainfall_2013,rainfall_2014,rainfall_2015,rainfall_2016,
+              rainfall_2017,rainfall_2018), fill = TRUE)
+
+
+datasetRainfall <- as.data.frame(rainfall);
+
+#Colocamos nombre a la segunda colomna para quitar el acento
+names(datasetRainfall)[2]<-"l/m2"
+##observamos que todos los datos se han unido, aplicando head y tail al dataset para ver cuando empiezan y acaban fechas
+head(datasetRainfall)
+tail(datasetRainfall)
--- Instrucciones.md
+++ Instrucciones.md
@@ -1,5 +1,7 @@
-# PASOS PARA REALZIAR EL ESUDIO
-## Recolocar datos de calidad del aire (Generando nuevo execel)
+# PASOS PARA REALZIAR EL ESTUDIO
+
+## Preparar datos
+### Recolocar datos de calidad del aire (Generando nuevo execel)
 Los datos deben estar contenidos en un excel con las siguientes columnas:
 	- Fecha 
 	- Estacion
@@ -11,11 +13,12 @@ Los datos deben estar contenidos en un excel con las siguientes columnas:
 	- O3
 	- BTX
 	- HC
-Para poder crear esta columnas debemos saber previamente, a que magnitud corresponde cada uno de ellos, para esto utilziar un siwtch y transformar.
+Asignar a los valores no validos un Na (se realiza por defecto), después, unirlos todos en un dataset
 
-Asignar a los valores no validos un Na
-
+### Recolocar datos precipitaciones
+Los datos delas precipitaciones deben estar en un excel con dos columnas, una fecha y otra la cantidad de precipitacion. Deben unirse en un dataset y cambiar el nombre de la 
+columna precipitacion para que si no esta en UTF-8, no falle.
 ## Comienza el estudio
 	- Cruzar datos de los años con las precipiatciones
 	- Observar influencia de las precipitaciones
-	- Elabroacion de graficas para visualziación de datos
+	- Elaboracion de graficas para visualziación de datos
